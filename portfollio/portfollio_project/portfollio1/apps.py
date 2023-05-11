bce3941992d2f889fcf92cb0a8a43f774c074f35
Ashoman <40643592+Ashoman@users.noreply.github.com>
2023-05-11 16:19:07 +0300

    Add files via upload

---
+++ portfollio/portfollio_project/portfollio1/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Portfollio1Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'portfollio1'
---
+++ portfollio/portfollio_project/portfollio1/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+path('Myportfollio/',views.Myportfollio,name='Myportfollio'),
+]
+ 
+ 
