709b57ba41121f6a12d936bbe0790da365530c11
Ashoman <40643592+Ashoman@users.noreply.github.com>
2023-05-20 13:43:38 +0300

    Add files via upload

---
+++ task-3/sprints_project/students/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Student
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("name", "phone", "address",)
+
+
+admin.site.register(Student, PostAdmin)
+# Register your models here.
