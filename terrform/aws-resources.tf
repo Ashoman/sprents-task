7f33096bc225e2d853b683fb6072d4c16cabf929
Ashoman <40643592+Ashoman@users.noreply.github.com>
2023-07-10 21:55:04 +0300

    Add files via upload

---
+++ terrform/aws-resources.tf
@@ -0,0 +1,125 @@
+# Create a VPC
+resource "aws_vpc" "dev" {
+  cidr_block = "10.0.0.0/16"
+   
+   tags = {
+     "Name"= "test"
+          
+    }   
+}
+
+# Creating subnet inside the vpc
+resource "aws_subnet" "sprints_subnet" {
+    vpc_id = aws_vpc.dev.id
+    cidr_block = "10.0.0.0/24"
+    tags = {
+        Name = "Sprints"
+    }
+}
+
+# Creating Internt gateway for this vpc
+resource "aws_internet_gateway" "sprints_gateway" {
+  vpc_id = aws_vpc.dev.id
+
+  tags = {
+    Name = "Sprints"
+  }
+}
+ 
+ # Creating a route table refering to the internet gateway
+resource "aws_route_table" "sprints_routet" {
+  vpc_id = aws_vpc.dev.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.sprints_gateway.id
+  }
+
+  tags = {
+    Name = "Sprints"
+  }
+}
+
+# Creating association to link route table with the subnet
+resource "aws_route_table_association" "sprints_rta" {
+  subnet_id      = aws_subnet.sprints_subnet.id
+  route_table_id = aws_route_table.sprints_routet.id
+}
+
+# Creating ami search to get the ubuntu ami id
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+# Creating a security group to allow http and https and ssh
+resource "aws_security_group" "sprints_sg" {
+  name        = "allow_http"
+  description = "Allow http inbound traffic"
+  vpc_id      = aws_vpc.dev.id
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "Sprints"
+  }
+}
+
+
+# Creating ec2 instance connected to all the created resources and providing a script to install apache2
+resource "aws_instance" "sprints_ec2" {
+    ami = data.aws_ami.ubuntu.id
+     instance_type = "t2.micro"
+    associate_public_ip_address = true
+    subnet_id = aws_subnet.sprints_subnet.id
+    security_groups = [aws_security_group.sprints_sg.id]
+      user_data = <<EOF
+#! /bin/bash
+sudo apt-get update
+sudo apt-get install -y apache2
+sudo systemctl start apache2
+sudo systemctl enable apache2
+EOF
+    tags = {
+        Name = "Sprints"
+    }
+
+}
